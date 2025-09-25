1f585a11f8cef673daced7190c7c5fe43e73b283
RGM-86RF <antoniokgage@gmail.com>
2025-09-25 09:39:36 -0400

    Proof of Concept

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,88 @@
+'use client'
 import Image from "next/image";
+import React, {useEffect , useState} from "react";
+
+
+const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+const [query, setQuery] = useState('');
+const [movies, setMovies] = useState([]);
+
+      useEffect(() => {
+        const fetchMovie = async () => {
+          try {
+            const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+            const data = await response.json();
+            setMovies(data.results); 
+          } catch (error) {
+            console.error('Error fetching movie:', error);
+          }
+        };
+        fetchMovie();
+      }, []);
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    alert(`You searched for: ${query}`); 
+   
+  };
+
+   
+  return (
+    <div className="flex flex-col min-h-screen">
+    <header className="flex items-center justify-between px-4 py-2 bg-[#333333]">
+        <div className="text-lime-300 font-bold text-x1">Mediapedia</div>
+        <form onSubmit={handleSubmit} className="flex-1 mx-4">
+          <input
+            type="search"
+            placeholder="Search to Explore..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="w-100 px-4 py-2 bg-[#1f1f1f] focus:outline-none focus:ring-2 focus:ring-lime-300"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        </form>
+        <div className="flex items-center space-x-4">
+          <button className=" text-lime-300 hover:underline">MENU</button>
         </div>
+      </header>
+      <section className="text-black px-20 py-40">
+       <h1 class="point">Welcome to Mediapedia</h1> 
+        
+      </section>
+
+    <section>
+    <div className="text-black font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+            <div className="overflow-x-auto"
+            style={{ width: '1400px' }}>
+              <div className="flex space-x-4 flex-nowrap"> 
+                {movies.map((movie) => (
+                  <div key={movie.id} className="flex-shrink-0 w-48 text-center">
+              <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              width={192}
+              height={288}
+              className="rounded shadow mx-auto"
+              />
+              <h2 className="text-lg font-bold mb-2">{movie.title}</h2>
+              </div>
+
+                ))}
+                </div>
+          </div>
+        
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </div>
+      <footer> 
+        <p>Mediapedia -Student Project- by Antonio Gage</p>
       </footer>
-    </div>
+    
+    </section>
+    
+  </div>
   );
 }
